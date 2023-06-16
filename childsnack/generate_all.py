636da909cf17cdf01778d7cb70ab4fcc7b3f9360
js <jsego6@gmail.com>
2023-06-16 16:44:04 +0200

    Issues #3 and #10: childsnack completed

--- childsnack/generate_all.py
+++ childsnack/generate_all.py
@@ -3,23 +3,36 @@ import logging
 
 
 def get_next_config(starting_children: int = 1,
-                    step_children: int = 1,
+                    max_children: int = 1,
                     starting_allergic: int = 0,
-                    step_allergic: int = 1,
+                    max_allergic: int = 1,
                     starting_trays: int = 1,
-                    step_trays: int = 1,
+                    max_trays: int = 1,
                     starting_sandwiches: int = 1,
-                    step_sandwiches: int = 1,
+                    max_sandwiches: int = 1,
                     out_folder: str = ".",
+                    starting_instance_id: int = 1,
                     max_instance_id: int = 100,
                     seed: int = 42):
-    instance_id, children, allergic, trays, sandwiches = \
-        1, starting_children, starting_allergic, starting_trays, starting_sandwiches
+    instance_id, steps = starting_instance_id, 0
+    num_instances = float(1+max_instance_id-starting_instance_id)
+    step_children = float(1+max_children-starting_children) / num_instances
+    step_allergic = float(1+max_allergic-starting_allergic) / num_instances
+    step_trays = float(1+max_trays-starting_trays) / num_instances
+    step_sandwiches = float(1+max_sandwiches-starting_sandwiches) / num_instances
     while instance_id <= max_instance_id:
+        children = int(step_children * steps + starting_children)
+        allergic = int(step_allergic * steps + starting_allergic)
+        trays = int(step_trays * steps + starting_trays)
+        sandwiches = int(step_sandwiches * steps + starting_sandwiches)
+        assert allergic <= children <= sandwiches
         print(f"c={children}; a={allergic}; t={trays}; s={sandwiches}")
         yield f"PYTHONHASHSEED=0 python childsnack.py -c {children} -a {allergic} -t {trays} -s {sandwiches} -o {out_folder} -i {instance_id} --seed {seed}"
         # Update input values for the next instance
         instance_id += 1
+        steps += 1
+        seed += 1
+        """
         if (sandwiches+step_sandwiches) <= children*2:
             sandwiches += step_sandwiches
         elif (trays+step_trays) <= min(children, 4):  # max_number_of_trays = 4
@@ -34,37 +47,45 @@ def get_next_config(starting_children: int = 1,
             sandwiches = max(children, starting_sandwiches)
             trays = starting_trays
             allergic = starting_allergic
+        """
     # raise StopIteration()
 
 
 def main():
-    starting_children = [1, 2, 25, 100]
-    step_children = [1, 1, 3, 50]
+    starting_children = [4, 4, 15, 50]
+    max_children = [10, 10, 40, 300]
     starting_allergic = [0, 0, 15, 50]
-    step_allergic = [1, 1, 10, 30]
-    starting_trays = [1, 1, 2, 2]
-    step_trays = [1, 1, 2, 2]
-    starting_sandwiches = [1, 2, 30, 100]
-    step_sandwiches = [1, 3, 20, 50]
+    max_allergic = [6, 6, 25, 150]  # approx max_children*0.5
+    starting_trays = [1, 1, 2, 4]
+    max_trays = [3, 3, 5, 10]
+    starting_sandwiches = [4, 4, 15, 50]
+    max_sandwiches = [15, 15, 60, 450]  # max_children * 1.5
     output_folders = ["training/easy", "testing/easy", "testing/medium", "testing/hard"]
     max_ids = [99, 30, 30, 30]
+    init_ids = [14, 1, 1, 1]  # 13 base cases
     seeds = [42, 1007, 1007, 1007]
     for experiment in range(4):
         # print(output_folders[experiment])
         for command in get_next_config(
                 starting_children=starting_children[experiment],
-                step_children=step_children[experiment],
+                max_children=max_children[experiment],
                 starting_allergic=starting_allergic[experiment],
-                step_allergic=step_allergic[experiment],
+                max_allergic=max_allergic[experiment],
                 starting_trays=starting_trays[experiment],
-                step_trays=step_trays[experiment],
+                max_trays=max_trays[experiment],
                 starting_sandwiches=starting_sandwiches[experiment],
-                step_sandwiches=step_sandwiches[experiment],
+                max_sandwiches=max_sandwiches[experiment],
                 out_folder=output_folders[experiment],
+                starting_instance_id=init_ids[experiment],
                 max_instance_id=max_ids[experiment],
                 seed=seeds[experiment]):
             ret_code = execute_command(command=command, shell=True)
             logging.info(f"Executed command={command}; result={ret_code}")
+
+    # Copy base cases
+    command = "cp base_cases/* training/easy/"
+    ret_code = execute_command(command=command, shell=True)
+    logging.info(f"Executed command={command}; result={ret_code}")
 
 
 if __name__ == "__main__":
--- childsnack/training/easy/p22.pddl
+++ childsnack/training/easy/p22.pddl
@@ -1,31 +1,39 @@
+;; children=4, allergic_children=0, trays=1, sandwiches=5, out_folder=training/easy, instance_id=22, seed=50
+
 (define (problem childsnack-22)
  (:domain childsnack)
  (:objects 
-    child1 child2 - child
-    tray1 tray2 - tray
-    sandw1 sandw2 sandw3 sandw4 - sandwich
-    bread1 bread2 - bread-portion
-    content1 content2 - content-portion
+    child1 child2 child3 child4 - child
+    tray1 - tray
+    sandw1 sandw2 sandw3 sandw4 sandw5 - sandwich
+    bread1 bread2 bread3 bread4 - bread-portion
+    content1 content2 content3 content4 - content-portion
     table1 table2 table3 - place
     )
  (:init 
     (at tray1 kitchen)
-    (at tray2 kitchen)
     (at_kitchen_bread bread1)
     (at_kitchen_bread bread2)
+    (at_kitchen_bread bread3)
+    (at_kitchen_bread bread4)
     (at_kitchen_content content1)
     (at_kitchen_content content2)
-    (allergic_gluten child2)
-    (allergic_gluten child1)
-    (no_gluten_bread bread2)
-    (no_gluten_bread bread1)
-    (no_gluten_content content1)
-    (no_gluten_content content2)
-    (waiting child2 table1)
+    (at_kitchen_content content3)
+    (at_kitchen_content content4)
+    (not_allergic_gluten child1)
+    (not_allergic_gluten child3)
+    (not_allergic_gluten child2)
+    (not_allergic_gluten child4)
     (waiting child1 table1)
+    (waiting child3 table3)
+    (waiting child2 table3)
+    (waiting child4 table1)
     (notexist sandw1)
     (notexist sandw2)
     (notexist sandw3)
-    (notexist sandw4))
+    (notexist sandw4)
+    (notexist sandw5))
  (:goal  (and (served child1)
-   (served child2))))
+   (served child2)
+   (served child3)
+   (served child4))))
